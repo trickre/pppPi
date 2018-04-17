87139be976bb501d2c4076f7d67ad0a704a3c3a6
ogino <ogino.nanomicro416@gmail.com>
2018-04-17 22:21:38 +0900

    pi numbers prepare test done

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+number_PI = []
+
+def main():
+    #1.照合用PI数列の用意
+    for pi in [3,1,4,1,5,9,2,6,5,3,5,8,9,7]:
+        number_PI.append(pi)
+        
+    print(number_PI)
+
+    #LOOP
+        #2.
+
+if __name__ == '__main__':
+    print("<< pppPi Python PROGRAM >>")
+    print("You can stop by \"Ctrl + C\"")
+    main()
