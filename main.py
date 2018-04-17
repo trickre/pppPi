9dd75194be3d3b3852da0de0617b90726394c25a
ogino <ogino.nanomicro416@gmail.com>
2018-04-17 22:27:18 +0900

    simple bone

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-number_PI = []
+number_PI = []  #照合する円周率の数列
+N_max = 10000   #最大試行回数
 
 def main():
     #1.照合用PI数列の用意
-    for pi in [3,1,4,1,5,9,2,6,5,3,5,8,9,7]:
+    for pi in [3,1,4,1,5,9,2,6,5]:
         number_PI.append(pi)
-        
-    print(number_PI)
-
     #LOOP
-        #2.
+        #2.randamに1文字出力
+        #3.1照合1段階
+            #3.2照合2段階
+                #3.3照合2段階
+                    #3.4照合2段階
 
 if __name__ == '__main__':
     print("<< pppPi Python PROGRAM >>")
